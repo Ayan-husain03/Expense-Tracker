2cb0c7ffd7a167cb72845b42f12845922571aa34
Ayan husain03 <hussainayan9897@gmail.com>
2025-07-11 15:47:29 +0530

    refactor: simplify password comparison in isPasswordCorrect method

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -26,7 +26,7 @@ userSchema.pre("save", async function (next) {
   next();
 });
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
+  return  bcrypt.compare(password, this.password);
 };
 userSchema.methods.generateToken = function () {
   const data = {
