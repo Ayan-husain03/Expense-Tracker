53d9563ede70efa92aebcfb3621f98c50c6046d4
Ayan husain03 <hussainayan9897@gmail.com>
2025-08-09 12:10:50 +0530

    feat: implement user authentication state management in authSlice

--- client/src/store/authSlice.js
+++ client/src/store/authSlice.js
@@ -1,13 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  user: null,
+  loading: true,
+};
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    createUser: () => {},
-    login: () => {},
-    logout: () => {},
+    setUser: (state, action) => {
+      state.user = action.payload.user;
+      state.loading = false;
+    },
+
+    logout: (state, action) => {
+      state.user = null;
+      state.loading = false;
+    },
   },
 });
+
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
